9b73b2308d6a061f273bb302e64632d4a7e10983
NancySDuguay <nancy.s.duguay@gmail.com>
2015-01-24 17:21:36 -0500

    Lantern Halo bulge

---
+++ LightSprite/Assets/Scripts/HaloBudge.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HaloBudge : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		light.range = Mathf.Abs(Mathf.Sin(Time.time)) + 1;
+	}
+}
