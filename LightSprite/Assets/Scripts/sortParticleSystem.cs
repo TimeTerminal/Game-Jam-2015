e1972391973b262014906a07caf106f3397daa54
NancySDuguay <nancy.s.duguay@gmail.com>
2015-01-24 16:13:58 -0500

    Adding lantern Prefab
    the lantern Prefab contains a particle system. Also, it has code to make
    sure the particles are behind the lantern. Also, two layers have been
    set up to accomplish that

---
+++ LightSprite/Assets/Scripts/sortParticleSystem.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sortParticleSystem : MonoBehaviour {
+	public string LayerName = "Particles";
+	
+	public void Start()
+	{
+		particleSystem.renderer.sortingLayerName = LayerName;
+	}
+}
