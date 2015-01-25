3587c3c5907f60c1f635852737b21675fadf886e
Travis Swan <t_r_z_swan@hotmail.com>
2015-01-24 19:44:17 -0500

    Stufff
    This weird

---
+++ LightSprite/Assets/Game.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+	public void StartGame() {
+				Debug.Log ("Clicked me");
+		Application.LoadLevel ("LightSpriteGame");
+		}
+}
