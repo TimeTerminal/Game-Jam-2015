bf3ccd089149c16ccddf9f2701f68875101998dc
Zach Sullivan <zach.r.sullivan@gmail.com>
2015-01-25 14:02:41 -0500

    Added audio

--- LightSprite/Assets/Scripts/LanternHit.cs
+++ LightSprite/Assets/Scripts/LanternHit.cs
@@ -5,14 +5,19 @@ public class LanternHit : MonoBehaviour {
 	GameObject player1Light;
 	GameObject player2Light;
 	
-	int score;
+
 	
 	public Font scoreFont;
 	Rect textArea = new Rect (Screen.width/2,10,Screen.width,Screen.height);
 
 	// Use this for initialization
 	void Start () {
-		score = 0;
+
+
+
+
+
+		
 		player1Light = GameObject.Find ("Player 1 Light");
 		player1Light.gameObject.GetComponent<Light>();
 
@@ -24,24 +29,20 @@ public class LanternHit : MonoBehaviour {
 	void Update () {
 	}
 	
-	void OnGUI() {
-		GUI.skin.font = scoreFont;
-		GUI.Label (textArea, "Score " + score);
-		
-	}
+
 	
 	void OnCollisionEnter2D(Collision2D collision){
 		GameObject isHit = collision.gameObject;
 		if (isHit.tag == "Player1") {
 			player1Light.light.spotAngle += 10;
 			this.transform.position = new Vector3 (Random.Range (0, 18), Random.Range (0, 13), 0);
-			score++;
+
 		}
 
 		if (isHit.tag == "Player2") {
 			player2Light.light.spotAngle += 10;
 			this.transform.position = new Vector3 (Random.Range (0, 18), Random.Range (0, 13), 0);
-			score++;
+
 		}
 
 		if(isHit.name == "Obstacle"){
--- LightSprite/Assets/Scripts/LightController.cs
+++ LightSprite/Assets/Scripts/LightController.cs
@@ -23,7 +23,13 @@ public class LightController : MonoBehaviour {
 			Debug.Log("Light is on!");
 			player1Light.light.spotAngle += 3;
 		}*/
-		
+		if(player1Light.light.spotAngle == 1){
+			Application.LoadLevel("gameOver Player 1");
+		}
+
+		if(player2Light.light.spotAngle == 1){
+			Application.LoadLevel("gameOver Player 2");
+		}
 		//Accelerate the shinking light over time
 		//currentSpeed = currentSpeed + (lightAcc * Time.deltaTime);
 		
