b41f863db8bfea59fbce6b792eea908272898d40
Zach Sullivan <zach.r.sullivan@gmail.com>
2015-01-24 19:42:48 -0500

    Added random obstacle spawning

--- LightSprite/Assets/Scripts/SpawnLevel.cs
+++ LightSprite/Assets/Scripts/SpawnLevel.cs
@@ -3,37 +3,86 @@ using System.Collections;
 
 public class SpawnLevel : MonoBehaviour {
 
+	public GameObject wallPrefab;
 	public GameObject obsPrefab;
+
 	GameObject newWall;
+	GameObject newObs;
 		
 	int gridWidth = 12;
 	int gridHeight = 6;
-	int xSpacing = 1;
+
+	public int obsCount;
+	public float PercentSpawn;
+
+	int[] numObs;
 
 	// Use this for initialization
-	void Start ()
-	{
-		int temp;
-		
-		for (int i=0; i < gridWidth; i++) //row
-		{
-			
-			for(int j=0; j < gridHeight;j++)//column
+	void Start () {
+
+		//numObs = new int[obsCount];
+
+		Random.seed = (int)System.DateTime.Now.Ticks;
+
+		int wallHeight;
+		int obsHeight;
+
+		int obsCounter = 0;
+		int currentISlot = 0;
+		int currentJSlot = 0;
+		  
+		//Iterate through and spawn a matrix of cube walls
+		//if(obsCounter < obsCount){
+		//while(obsCounter < obsCount){
+			for (int i = 0; i < gridWidth; i++) //row
 			{
-				newWall = Instantiate(obsPrefab, new Vector3 (i,j,0),Quaternion.identity) as GameObject;
-				newWall.transform.parent =gameObject.transform;
-				temp=j;
-				newWall.transform.position+=new Vector3(i+1,temp+1,0); //THIS LINE
+				for(int j = 0; j < gridHeight; j++)//column
+				{
+
+
+						if(randomBoolean() == true){
+						//i += 3;
+						//j += 3;
+						newWall = Instantiate(wallPrefab, new Vector3 (i, j, 0), Quaternion.identity) as GameObject;
+						newWall.transform.parent = gameObject.transform;
+						wallHeight = j;
+						newWall.transform.position +=new Vector3(i + 1, wallHeight + 1, 0); //THIS LINE
+
+						obsCounter++;
+						
+					}
+				}
+				
+			}
+		//}
+
+		//Iterate through and spawn a matrix of obstcales to block the player
+		/*for (int i = 0; i < gridWidth; i++) //row
+		{
+			for(int j = 0; j < gridHeight; j++){
+
+				newObs = Instantiate(obsPrefab, new Vector3 (i, j, 0), Quaternion.identity) as GameObject;
+				newObs.transform.parent = gameObject.transform;
+				obsHeight = j;
+				newObs.transform.position+=new Vector3(i + 2, obsHeight + 2, 0); //THIS LINE
+
 			}
 			
 			
-		}
-		gameObject.transform.position = new Vector3(0f,0f,0f); 
+		}*/
+
 	}
 
 	
-	// Update is called once per frame
-	void Update () {
-	
+	bool randomBoolean ()
+	{
+
+
+
+		if (Random.value <= PercentSpawn)
+		{
+			return true;
+		}
+		return false;
 	}
 }
