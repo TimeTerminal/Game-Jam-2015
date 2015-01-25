40f7a3cbad5d030f7cebdf25d810a7d2041ca3d5
NancySDuguay <nancy.s.duguay@gmail.com>
2015-01-25 11:38:57 -0500

    Add Animation

--- LightSprite/Assets/Scripts/LightController.cs
+++ LightSprite/Assets/Scripts/LightController.cs
@@ -2,34 +2,33 @@
 using System.Collections;
 
 public class LightController : MonoBehaviour {
-
-	GameObject playerLight;
-
+	public GameObject playerLight;
+	
 	//public float currentSpeed = 0.0f;
-
+	
 	//public float lightAcc = 0.2f;
-
+	
 	
 	// Use this for initialization
 	void Start () {
+		
 
-		playerLight = GameObject.Find ("Player Light");
-		playerLight.gameObject.GetComponent<Light>();
+		//playerLight.gameObject.GetComponent<Light>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		
 		if (Input.GetButtonDown ("TurnOnLight")) {
 			Debug.Log("Light is on!");
 			playerLight.light.spotAngle += 3;
 		}
-
+		
 		//Accelerate the shinking light over time
 		//currentSpeed = currentSpeed + (lightAcc * Time.deltaTime);
-
+		
 		float scale = Time.deltaTime * 2;
-
+		
 		playerLight.light.spotAngle -= scale;
 	}
 }
--- LightSprite/Assets/Scripts/playerControls.js
+++ LightSprite/Assets/Scripts/playerControls.js
@@ -5,36 +5,101 @@ var moveRight : KeyCode;
 var moveUp : KeyCode;
 var moveDown : KeyCode;
 
+var charLeft : Sprite;
+var charRight : Sprite;
+var charBottom : Sprite;
+var charUp : Sprite;
+
+
 var speed : float = 10;
 
+var player : Animator;
+
+function Start (){
+	player = GetComponent (Animator);
+}
+
+function Awake (){
+	player = GetComponent (Animator);
+}
+
+function TurnAllStatesFalse ()
+{
+	player.SetBool ("ReleaseUp", false);
+	player.SetBool ("ReleaseDown", false);
+	player.SetBool ("ReleaseRight", false);
+	player.SetBool ("ReleaseLeft", false);
+	
+	player.SetBool ("PressUp", false);
+	player.SetBool ("PressDown", false);
+	player.SetBool ("PressRight", false);
+	player.SetBool ("PressLeft", false);
+}
 
 function Update () {
 
-	if (Input.GetKey(moveLeft)) {
-		rigidbody2D.velocity.x = speed * -1;
+	//Left
+	if (Input.GetKey(moveLeft) == true) {
+		transform.position += Vector3.left * Time.deltaTime;
+		TurnAllStatesFalse ();
+		player.SetBool ("PressLeft", true);
+		/*transform.rigidbody2D.velocity.x = speed * -1;
+		rigidbody.AddForce (Vector3.left,  -10);*/
 	}
-	else if (Input.GetKey(moveRight)) {
-		rigidbody2D.velocity.x = speed;
+	else if (Input.GetKeyUp (moveLeft)){
+		TurnAllStatesFalse ();
+		player.SetBool ("ReleaseLeft", true);
 	}
-	else if (Input.GetKey(moveUp)) {
-		rigidbody2D.velocity.y = speed;
+	
+	//Right
+	if (Input.GetKey (moveRight)) {
+		
+		TurnAllStatesFalse ();
+		player.SetBool ("PressRight", true);
+		transform.position += Vector3.right * Time.deltaTime;
+		//transform.rigidbody2D.velocity.x = speed * 1;
 	}
-	else if (Input.GetKey(moveDown)) {
-		rigidbody2D.velocity.y = speed * -1;
+	else if (Input.GetKeyUp (moveRight)){
+		TurnAllStatesFalse ();
+		player.SetBool ("ReleaseRight", true);
+		//transform.rigidbody2D.velocity.x = speed * 1;
 	}
-	else {
 
+	//Up
+	if (Input.GetKey (moveUp)) {
+		TurnAllStatesFalse ();
+		player.SetBool ("PressUp", true);
+		transform.position += Vector3.up * Time.deltaTime;
+	}
+	else if (Input.GetKeyUp (moveUp)){
+		TurnAllStatesFalse ();
+		player.SetBool ("ReleaseUp", true);
+	}
+	
+	//Down
+	if (Input.GetKey (moveDown)) {
+		transform.position += Vector3.down * Time.deltaTime;
+		TurnAllStatesFalse ();
+		player.SetBool ("PressDown", true);
+	}
+	else if (Input.GetKeyUp (moveDown)){
+		TurnAllStatesFalse ();
+		player.SetBool ("ReleaseDown", true);
+	}
+	/* ============== */
+	
+	//Normal State
+	else {
 		rigidbody2D.velocity.x = 0;
 		rigidbody2D.velocity.y = 0;
 	}
-
 }
 
-function OnCollisionEnter(collision : Collision)
+/*function OnCollisionEnter(collision : Collision)
 {
 	var whohitme = collision.gameObject;
 	if( whohitme.name == "lantern") {
 		Destroy(whohitme);
 	}
 }
-
+*/
